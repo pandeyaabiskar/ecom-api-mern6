76755a5a70763b9e276f48987d83db1327cab8fe
Aabiskar Pandey <pandeyaabiskar@gmail.com>
2023-04-23 08:10:59 +0545

    Validation

--- models/product.js
+++ models/product.js
@@ -1,10 +1,24 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  price: Number,
-  category: String,
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+    minlength: [10, "Title should be at least 10 characters"],
+    maxlength: [100, "Title cannot be greater than 100 characters"]
+  },
+  description: {
+    type: String
+  },
+  price: {
+    type: Number,
+    required: [true, "Price is required"],
+    min: [0, "Price cannot be less than 0"]
+  },
+  category: {
+    type: String,
+    enum: ['jewelery', 'electronics', "men's clothing", "women's clothing"]
+  },
   image: String,
   rating: {
     rate: Number,
